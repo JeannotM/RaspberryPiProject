f68edb1a3f168b64ff04c0875a9005fecdfb2763
filmonda <59563760+filmonda@users.noreply.github.com>
2022-12-16 11:56:43 +0100

    de laatste versie

--- RaspberryPi_code/main_auto_watering.py
+++ RaspberryPi_code/main_auto_watering.py
@@ -22,6 +22,79 @@ def get_status(pin = 12):
     return GPIO.input(pin) 
     
 
+# External module imp
+import RPi.GPIO as GPIO
+import datetime
+import time
+from ADC1115_materlevel_sensor import get_moisture_level, get_water_level
+from ultrasonic_water_level import get_distance
+init = False
+
+#GPIO.setmode(GPIO.BCM) # Broadcom pin-numbering scheme
+GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BCM)
+GPIO.setup(26,GPIO.OUT) # reley
+GPIO.setup(12,GPIO.IN) #sensor mosi
+def get_last_watered():
+    try:
+        f = open("last_watered.txt", "r")
+        return f.readline()
+    except:
+        return "NEVER!"
+   
+def get_status(pin = 12):
+    GPIO.setup(pin, GPIO.IN)
+    return GPIO.input(pin) 
+    
+
+
+def init_output(pin):
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.LOW)
+    # GPIO.output(pin, GPIO.HIGH) 
+
+def should_pump_water(delay = .6, pump_pin =26, water_sensor_pin = 12):
+    consecutive_water_count = 0 
+    init_output(pump_pin)
+    try:
+        # and consecutive_water_count < 1
+        while 1:
+            dis_waterl_level=get_distance
+            level = get_moisture_level()
+            print(level, consecutive_water_count)
+
+            # 1200 is waterlevel
+            if level > 2000 and get_distance() > 2:
+                if consecutive_water_count < 10:
+                    GPIO.output(pump_pin, GPIO.HIGH)
+                else:
+                    GPIO.output(pump_pin, GPIO.LOW)
+                consecutive_water_count += 1
+            else:
+                GPIO.output(pump_pin, GPIO.HIGH)
+                consecutive_water_count = 0
+
+            time.sleep(delay)
+
+
+    except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+        GPIO.cleanup() # cleanup all GPI
+
+
+
+def pump_on(pump_pin = 26, delay = 1):
+    init_output(pump_pin)
+    f = open("last_watered.txt", "w")
+    f.write("Last watered {}".format(datetime.datetime.now()))
+    f.close()
+    GPIO.output(pump_pin, GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(pump_pin, GPIO.LOW)
+
+if __name__ == "__main__":
+    should_pump_water()
+    
+  
 
 def init_output(pin):
     GPIO.setup(pin, GPIO.OUT)
--- RaspberryPi_code/ultrasonic_water_level_sensor.py
+++ RaspberryPi_code/ultrasonic_water_level_sensor.py
@@ -1,6 +1,6 @@
-# er komt nog de code
 import RPi.GPIO as GPIO
 import time
+print("x")
 import time,os
 
 import datetime
@@ -9,8 +9,8 @@ TRIG = 21  #altra sonic GPIO
 ECHO = 20
 ALARM = 23  #alarm die komt nog
 #stel de tank lengte is 100
-TANK_VOEL = 100
-TANK_LAAG = 40
+TANK_VOEL = 14
+TANK_LAAG = 2
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -23,16 +23,19 @@ GPIO.setup(ALARM,GPIO.OUT)
 GPIO.output(ALARM, True)
 
 print ("Waiting For Sensor To start")
-time.sleep(1)
+time.sleep(1) 
 
 def get_distance():
 	dist_add = 0
-	for x in range(20):
+	x = 0
+	while 1:
+	#for x in range(20):
+	
 		try:
 			GPIO.output(TRIG, True)
 			time.sleep(0.00001)
 			GPIO.output(TRIG, False)
-
+			
 			while GPIO.input(ECHO)==0:
 				start_meeting = time.time()
 
@@ -40,20 +43,21 @@ def get_distance():
 				stop_meeting = time.time()
 
 			meeting_time = stop_meeting - start_meeting
-
+			
 			distance = meeting_time * 17150
 
 			distance = round(distance, 3)
 			print (x, "distance: ", distance)
-
+		
 			dist_add = dist_add + distance
 			#print "dist_add: ", dist_add
 			time.sleep(.1) # 100ms interval between readings
-
-		except Exception as e:
-
+			low_level_warning()
+		
+		except Exception as e: 
+		
 			pass
-
+	
 	avg_dist=dist_add/20
 	dist=round(avg_dist,3)
 	#print ("dist: ", dist)
@@ -66,10 +70,10 @@ def get_distance():
 	result=os.popen(cmd).read()
 	print (cmd)
 	 """
-
-
+		
+	
 def low_level_warning(dist):
-	tank_height=TANK_VOEL     #set  tank height here
+	tank_height=TANK_VOEL     #set your tank height here
 	level=tank_height-dist
 	if(level<TANK_LAAG):
 		print("level low : ", level)
@@ -80,18 +84,19 @@ def low_level_warning(dist):
         #hier komt nog Green LED
 		print("level ok")
 
-
+		
 
 def main():
-
+	
 	distance=get_distance()
-
+	
 	print ("distance: ", distance)
 	#sendData_to_remoteServer(distance)
 	low_level_warning(distance)
 	print ("---------------------")
-
-
-
+    
+	
+	
 if __name__ == '__main__':
     main()
+
